67e230c397c1608e8f1f35cba4832f5a98e2166a
Mateusz Paprocki <mattpap@gmail.com>
2020-10-13 19:26:44 +0200

    Add support for CustomJSExpr

--- bokehjs/src/lib/core/has_props.ts
+++ bokehjs/src/lib/core/has_props.ts
@@ -233,6 +233,7 @@ export abstract class HasProps extends Signalable() implements Equals, Printable
   readonly destroyed       = new Signal0<this>(this, "destroyed")
   readonly change          = new Signal0<this>(this, "change")
   readonly transformchange = new Signal0<this>(this, "transformchange")
+  readonly exprchange      = new Signal0<this>(this, "exprchange")
 
   readonly properties: {[key: string]: Property} = {}
 
@@ -305,8 +306,11 @@ export abstract class HasProps extends Signalable() implements Equals, Printable
 
   finalize(): void {
     for (const prop of this) {
-      if (prop.spec.transform != null)
-        this.connect(prop.spec.transform.change, () => this.transformchange.emit())
+      const {transform, expr} = prop.spec
+      if (transform != null)
+        this.connect(transform.change, () => this.transformchange.emit())
+      if (expr != null && "change" in Object(expr))
+        this.connect(expr.change, () => this.exprchange.emit())
     }
 
     this.initialize()
